8b64d30b109d59e87cb06d92597d7a284ef91711
Joachim Fuchs <56673765+joachimfuchs1067@users.noreply.github.com>
2021-06-01 22:28:11 +0200

    easy mock for mongodb added

--- simpleapp/src/test/java/simpleapp/MongoDbAdderIT.java
+++ simpleapp/src/test/java/simpleapp/MongoDbAdderIT.java
@@ -1,5 +1,35 @@
 package simpleapp;
 
+import org.bson.Document;
+import org.easymock.EasyMock;
+import org.junit.jupiter.api.Test;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+/**
+ * https://stackoverflow.com/questions/7413985/unit-testing-with-mongodb
+ * 
+ * @author JFUCHS
+ *
+ */
 public class MongoDbAdderIT {
 
+	@Test
+	public void test() {
+		MongoClient mongo = EasyMock.mock(MongoClient.class);
+	    MongoDatabase database = EasyMock.mock(MongoDatabase.class);
+		MongoCollection<Document> databaseCollection = EasyMock.mock(MongoCollection.class);
+
+		EasyMock.expect(mongo.getDatabase("foo")).andReturn(database);
+		EasyMock.expect(database.getCollection("bar")).andReturn(databaseCollection);
+
+		EasyMock.replay(mongo);
+		EasyMock.replay(database);
+		
+		MongoDatabase mongoDatabase = mongo.getDatabase("foo");
+		MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("bar");
+	}
+	
 }
